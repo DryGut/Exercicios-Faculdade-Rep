8c9626b7bb08c8af5e02b5e6cffa66878da9b822
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-08-02 16:20:56 +0000

    atualização do projeto integrado

--- .ccls-cache/@home@runner@Exercicios-Faculdade-Rep/Aulas e Exercicios@projetointegradoII.c
+++ .ccls-cache/@home@runner@Exercicios-Faculdade-Rep/Aulas e Exercicios@projetointegradoII.c
@@ -3,45 +3,91 @@
 
 //constante para definir o tamanho das strings
 #define TAM_S 50
+//escopo da função
+struct cadastro* montar_cad();
 //cria a estrutura do formulário a ser preenchido
-struct cadastro{
+typedef struct cadastro{
 char name[TAM_S];
 char address[TAM_S];
 char phone[TAM_S];
-};
-//função para criar o array
+}c;
+//função para criar um vetor que receberá os dados da struct
 struct cadastro* montar_cad(){
  
-  //cria um array e define uma quantidade de índices e sua alocação na memória
-  struct cadastro *c = (struct cadastro*) malloc(2*sizeof(struct cadastro));
-  /* conforme o valor definido para o índice
-   * é usado um loop for para gerar os valores
-   * que serão armazenados dentro do escopo da 
-   * struct definido anteriormente.
+  //cria o vetor e sua alocação na memória
+  struct cadastro *c = (struct cadastro*) malloc(sizeof(struct cadastro));
+  /*
+   * bloco que irá coletar os dados
    */
-  for(int i=0; i<=1; i++){
-    printf("Nome: ");
-    fflush(stdin);
-    fgets(c[i].name, TAM_S, stdin);
-    printf("Endereço: ");
-    fflush(stdin);
-    fgets(c[i].address, TAM_S, stdin);
-    printf("Telefone: ");
-    fflush(stdin);
-    fgets(c[i].phone, TAM_S, stdin);
-  }
+  system("echo \033[93m"); //cor vermelha na tela de inserção de dados
+  printf("\n****** INSIRA OS DADOS ******\n");
+ 
+  printf("Nome: ");
+  fflush(stdin);
+  fgets(c->name, TAM_S, stdin);
+  printf("Endereço: ");
+  fflush(stdin);
+  fgets(c->address, TAM_S, stdin);
+  printf("Telefone: ");
+  fflush(stdin);
+  fgets(c->phone, TAM_S, stdin);
+
   return c;
 }
 //programa principal
 int main(){
+  //cria a variável da estrutura
+  struct cadastro *c;
+  //variável para o arquivo que será utilizado
+  FILE *arq;
+  int opcao;
   
-  struct cadastro *c = montar_cad();
-  /* imprime de forma organizada os dados
-   * que foram inseridos na função montar_cad() 
-   */
-  printf("\n###### DADOS PESSOAIS ######");
-  for(int i=0; i<2; i++){
-    printf("\nNome: %sEndereço: %sTelefone: %s", c[i].name, c[i].address, c[i].phone);
-  }
+  do{
+    system("echo \033[92m");//define a cor do menu
+    printf("=================================");
+    printf("\n|               MENU            |");
+    printf("\n=================================");
+    printf("\n|   Selecione a Opção Desejada  |");
+    printf("\n|    [1] - Para novo Cadastro   |");
+    printf("\n|    [2] - Para inclusão        |");
+    printf("\n|    [3] - Para SAIR            |");
+    printf("\n=================================");
+    printf("\nDigite a Opção Desejada: ");
+    scanf("%d", &opcao);
+    getchar();//limpa o buffer
+    switch(opcao){
+      case 1:
+        c = montar_cad(); //chamada da função que coleta os dados
+        system("clear");//limpa a tela dps da inclusão dos dados
+        arq = fopen("cad1.txt", "w");//abre e escreve no arquivo com a flag W
+        
+        //formatação da primeira inclusão no arquivo
+        fprintf(arq, "###### DADOS PESSOAIS ######");
+        fprintf(arq, "\nNome: ");
+        fputs(c->name, arq);
+        fprintf(arq, "Endereço: ");
+        fputs(c->address, arq);
+        fprintf(arq, "Idade: ");
+        fputs(c->phone, arq);
+        fclose(arq);
+        break;
+
+      case 2:
+        c = montar_cad();//chamada da função que coleta os dados
+        system("clear");
+        arq = fopen("cad1.txt", "a");
+        //com a flag A toda nova inclusão será ao final do arquivo
+        fprintf(arq, "\nNome: ");
+        fputs(c->name, arq);
+        fprintf(arq, "Endereço: ");
+        fputs(c->address, arq);
+        fprintf(arq, "Idade: ");
+        fputs(c->phone, arq);
+        fclose(arq);
+
+      case 3:
+        break;
+    }
+  }while(opcao != 3);
   return 0;
 }
